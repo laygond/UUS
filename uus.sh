025b65d1acf5c3abbf1a0734e8e010313bd62846
laygond <laygond_bryan@hotmail.com>
2019-05-11 08:09:04 +0000

    added tensorflow

--- uus.sh
+++ uus.sh
@@ -2,23 +2,37 @@
 # $ sudo chmod +x uss.sh
 # $ ./uss.sh
 
-# Script starts here
+# Update and upgrade packages
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get -y install python3-pip
+# Install pip3
+sudo apt-get install python3-pip
 
-sudo pip3 install virtualenv virtualenvwrapper
+# Install virtual environment and set environmental variables
+sudo pip3 install -U virtualenv virtualenvwrapper
 #$ sudo rm -rf ~/get-pip.py ~/.cache/pip
 echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
 echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
 source ~/.bashrc
-mkvirtualenv py3cv4 -p python3
 
-workon py3cv4
+# Create Virtual Environment and activate it
+mkvirtualenv udacity -p python3
+workon udacity
+#virtualenv --system-site-packages -p python3 ./venv
+#source ./venv/bin/activate  # sh, bash, ksh, or zsh
+
+# Install pkg within the udacity env
+pip3 install --upgrade pip
+pip3 list  # show packages installed within the virtual environment
+
+# Install OpenCV 
 pip3 install opencv-contrib-python
+
+# Install TensorFlow
+pip3 install tensorflow-gpu
 
 # { # try
 
