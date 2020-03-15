69e0b25007702ba56223f73c242cb0837105d5f6
laygond <laygond_bryan@hotmail.com>
2020-03-14 22:24:46 -0400

    :sparkles: feat: shortcut to paper notes

---
+++ .my_custom_unix_commands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# my reference:
+# https://medium.com/devnetwork/how-to-create-your-own-custom-terminal-commands-c5008782a78e
+
+
+# --------- CUSTOM COMMANDS by Bryan Laygond (personal) -----------
+
+# Open Dropbox Paper Notes in Chrome
+function paper() {
+  google-chrome https://paper.dropbox.com/folder/show/Tutorials-e.1gg8YzoPEhbTkrhvQwJ2zzxOY3ofXnw0n4dMNrcQppzTfcD5Utlp
+}
