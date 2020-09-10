be23ed3e4ac3e0a51db6bd5142720817f230ee90
laygond <laygond_bryan@hotmail.com>
2020-09-09 20:17:14 -0400

    :sparkles: device paths in usbinfo

--- .my_custom_unix_commands.sh
+++ .my_custom_unix_commands.sh
@@ -48,8 +48,20 @@ function osinfo() {
 }
 
 # Check USB info
+# from https://unix.stackexchange.com/questions/144029/command-to-determine-ports-of-a-device-like-dev-ttyusb0
 function usbinfo() {
   sudo cat /sys/kernel/debug/usb/devices
+
+  for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+  (
+    syspath="${sysdevpath%/dev}"
+    devname="$(udevadm info -q name -p $syspath)"
+    [[ "$devname" == "bus/"* ]] && exit
+    eval "$(udevadm info -q property --export -p $syspath)"
+    [[ -z "$ID_SERIAL" ]] && exit
+    echo "/dev/$devname - $ID_SERIAL"
+  )
+  done
 }
 
 # Check CUDA info
